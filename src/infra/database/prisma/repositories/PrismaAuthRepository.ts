5e2fb01dd9a2fce1f583c77ff422bc817fd10f98
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-07-13 19:27:04 -0300

    feat: add prisma auth repository

---
+++ src/infra/database/prisma/repositories/PrismaAuthRepository.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/domain/entities/User';
+import { AuthRepository } from 'src/domain/repositories/AuthRepository';
+import { PrismaService } from '../prisma.service';
+import { PrismaUserMapper } from '../mappers/PrismaUserMapper';
+
+@Injectable()
+export class PrismaAuthRepository implements AuthRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async register(user: User): Promise<void> {
+    const userToPrisma = PrismaUserMapper.toPrisma(user);
+
+    await this.prisma.user.create({
+      data: userToPrisma,
+    });
+  }
+}
