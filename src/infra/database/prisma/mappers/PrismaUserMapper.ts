cc2556fe2b23bfa761d75908288b7071562c6504
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-07-13 19:26:52 -0300

    feat: add mapper prisma user

---
+++ src/infra/database/prisma/mappers/PrismaUserMapper.ts
@@ -0,0 +1,17 @@
+import { User } from 'src/domain/entities/User';
+import { User as RawUser } from '@prisma/client';
+
+export class PrismaUserMapper {
+  static toPrisma(user: User): RawUser {
+    return {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      password: user.password,
+      avatar_url: user.avatar_url,
+      verified: user.verified,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+  }
+}
