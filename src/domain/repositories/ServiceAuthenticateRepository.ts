f48e9711ac46b1b815c93bfd3952b128c2385c17
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-07-15 20:25:35 -0300

    feat: add service authenticate repository

--- src/domain/repositories/ServiceAuthenticateRepository.ts
+++ src/domain/repositories/ServiceAuthenticateRepository.ts
@@ -2,4 +2,5 @@ import { User } from '../entities/User';
 
 export abstract class ServiceAuthenticateRepository {
   abstract authenticate(user: User): Promise<string>;
+  abstract decode(token: string): Promise<boolean>;
 }
