78d1f1c2e5b94b62a315f99eac5c313ba3acb750
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-07-15 20:25:11 -0300

    fet: add jwt service authenticate

---
+++ src/application/services/authenticate/jwt.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/domain/entities/User';
+import { ServiceAuthenticateRepository } from 'src/domain/repositories/ServiceAuthenticateRepository';
+import jwt from 'jsonwebtoken';
+import { JwtPayloadService } from 'src/domain/interfaces/JwtPayloadService';
+
+@Injectable()
+export class JwtService implements ServiceAuthenticateRepository {
+  async authenticate(user: User): Promise<string> {
+    const payload: JwtPayloadService = {
+      id: user.id,
+      email: user.email,
+      password: user.password,
+    };
+
+    const token = jwt.sign(payload, `${process.env.SECRET_KEY_JWT}`, {
+      expiresIn: '7d',
+    });
+
+    return token;
+  }
+  async decode(token: string): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+}
