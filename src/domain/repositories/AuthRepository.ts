d7601ede7d23a7023e7811f0057f08937ac4e3d7
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-07-16 02:53:02 -0300

    feat: add login in auth repository

--- src/domain/repositories/AuthRepository.ts
+++ src/domain/repositories/AuthRepository.ts
@@ -2,4 +2,5 @@ import { User } from '../entities/User';
 
 export abstract class AuthRepository {
   abstract register(user: User): Promise<void>;
+  abstract login(email: string, password: string): Promise<User>;
 }
