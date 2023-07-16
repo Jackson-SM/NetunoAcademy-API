49ddae9480f4ba5681fe1228a7ca988247ec1e89
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-07-16 02:53:23 -0300

    feat: add login in prisma auth

--- src/infra/database/prisma/repositories/PrismaAuthRepository.ts
+++ src/infra/database/prisma/repositories/PrismaAuthRepository.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { User } from 'src/domain/entities/User';
 import { AuthRepository } from 'src/domain/repositories/AuthRepository';
 import { PrismaService } from '../prisma.service';
 import { PrismaUserMapper } from '../mappers/PrismaUserMapper';
-import { ServiceImageRepository } from 'src/domain/repositories/ServiceImageRepository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class PrismaAuthRepository implements AuthRepository {
@@ -15,5 +15,25 @@ export class PrismaAuthRepository implements AuthRepository {
     await this.prisma.user.create({
       data: userToPrisma,
     });
+  }
+
+  async login(email: string, password: string): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordIsValid) {
+      throw new HttpException('User is invalid', HttpStatus.NOT_FOUND);
+    }
+
+    return PrismaUserMapper.toDomain(user);
   }
 }
