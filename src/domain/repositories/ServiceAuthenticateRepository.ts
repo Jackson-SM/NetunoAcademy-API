1fc740a3694b35ea9ddd2ef3cd923a7bc417dce4
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-07-27 15:23:12 -0300

    fix: fixed return in decode

--- src/domain/repositories/ServiceAuthenticateRepository.ts
+++ src/domain/repositories/ServiceAuthenticateRepository.ts
@@ -1,6 +1,7 @@
 import { User } from '../entities/User';
+import { JwtPayloadService } from '../interfaces/JwtPayloadService';
 
 export abstract class ServiceAuthenticateRepository {
   abstract authenticate(user: User): Promise<string>;
-  abstract decode(token: string): Promise<boolean>;
+  abstract decode(token: string): Promise<JwtPayloadService>;
 }
