f3af733433c67acfe29468267d9a2a5c58a488a7
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-04 02:24:40 -0300

    feat: apply middleware in auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { HttpModule } from './infra/http/http.module';
+import { LoggerMiddleware } from './infra/http/middlewares/LoggerMiddleware';
 
 @Module({
   imports: [HttpModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('auth');
+  }
+}
