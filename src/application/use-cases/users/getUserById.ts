1dcd0c38b98e1f55e7d051a705bde7ebee5b60ac
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-19 21:03:22 -0300

    feat: add getById use case

---
+++ src/application/use-cases/users/getUserById.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/domain/entities/User';
+import { UserRepository } from 'src/domain/repositories/UserRepository';
+
+interface GetUserByIdCaseRequest {
+  id: string;
+}
+
+interface GetUserByIdCaseResponse {
+  user: User;
+}
+
+@Injectable()
+export class LoginUseCase {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute(
+    request: GetUserByIdCaseRequest,
+  ): Promise<GetUserByIdCaseResponse> {
+    const { id } = request;
+
+    const user = await this.userRepository.getById(id);
+
+    return {
+      user,
+    };
+  }
+}
