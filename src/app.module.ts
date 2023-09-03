f1b33440b14a22a074ab639b12e63955cf3255f0
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-09-03 18:52:01 -0300

    feat: add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { HttpModule } from './infra/http/http.module';
-import { LoggerMiddleware } from './infra/http/middlewares/LoggerMiddleware';
 
 @Module({
   imports: [HttpModule],
   controllers: [],
   providers: [],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('auth');
-  }
-}
+export class AppModule {}
--- src/infra/http/controllers/UserController/user.module.ts
+++ src/infra/http/controllers/UserController/user.module.ts
@@ -1,12 +1,25 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { GetUserByIdController } from './GetUserByIdController';
 import { GetUserByIdCase } from 'src/application/use-cases/users/get-user-by-id-case';
 import { DatabaseModule } from 'src/infra/database/database.module';
+import { LoggerMiddleware } from '../../middlewares/LoggerMiddleware';
+import { ServiceAuthenticateRepository } from 'src/domain/repositories/ServiceAuthenticateRepository';
+import { JwtService } from 'src/application/services/authenticate/jwt.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [GetUserByIdController],
-  providers: [GetUserByIdCase],
+  providers: [
+    GetUserByIdCase,
+    {
+      provide: ServiceAuthenticateRepository,
+      useClass: JwtService,
+    },
+  ],
   exports: [],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('users');
+  }
+}
