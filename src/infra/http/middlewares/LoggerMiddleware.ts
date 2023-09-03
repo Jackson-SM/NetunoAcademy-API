bcf9a75749e5ebde65178c8e3c4acd2b20ff8c91
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-09-03 19:10:02 -0300

    feat: implement logger middleware

--- src/infra/http/middlewares/LoggerMiddleware.ts
+++ src/infra/http/middlewares/LoggerMiddleware.ts
@@ -1,10 +1,24 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { ServiceAuthenticateRepository } from 'src/domain/repositories/ServiceAuthenticateRepository';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...');
-    next();
+  constructor(private jwtService: ServiceAuthenticateRepository) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    try {
+      const token = req.cookies['access-token'];
+
+      await this.jwtService.decode(token);
+
+      next();
+    } catch (err) {
+      throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+    }
   }
 }
