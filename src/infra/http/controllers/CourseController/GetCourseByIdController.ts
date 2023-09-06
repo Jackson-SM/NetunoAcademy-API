b115c64023cd2deefb7e31aa8df21d469f0408ce
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-09-05 23:34:02 -0300

    feat: add parse pipoe in course route getbyid

--- src/infra/http/controllers/CourseController/GetCourseByIdController.ts
+++ src/infra/http/controllers/CourseController/GetCourseByIdController.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { GetCourseByIdUseCase } from 'src/application/use-cases/courses/get-course-by-id-use-case';
 
 @Controller('courses')
@@ -6,7 +12,13 @@ export class GetCourseByIdController {
   constructor(private getCourseByIdUseCase: GetCourseByIdUseCase) {}
 
   @Get(':id')
-  async handler(@Param('id') id: string) {
+  async handler(
+    @Param(
+      'id',
+      new ParseUUIDPipe({ errorHttpStatusCode: HttpStatus.BAD_REQUEST }),
+    )
+    id: string,
+  ) {
     return `get course by id ${id}`;
   }
 }
