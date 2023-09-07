8e2b6297db492a8b1b0411fc8d6680c0e758e0c4
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-09-06 21:54:09 -0300

    feat: add service authenticate repository verify

--- src/domain/repositories/ServiceAuthenticateRepository.ts
+++ src/domain/repositories/ServiceAuthenticateRepository.ts
@@ -1,7 +1,9 @@
+import { JwtPayload } from 'jsonwebtoken';
 import { User } from '../entities/User';
 import { JwtPayloadService } from '../interfaces/JwtPayloadService';
 
 export abstract class ServiceAuthenticateRepository {
   abstract authenticate(user: User): Promise<string>;
   abstract decode(token: string): Promise<JwtPayloadService>;
+  abstract verify(token: string): Promise<string | JwtPayload>;
 }
