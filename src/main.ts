f0dc760d2d664fc5a6cc2d9e869ccc95140a2f09
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-09-06 21:50:03 -0300

    feat: use cookie parser in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   config();
+  app.use(cookieParser());
   cloudinary.config({
     cloud_name: `${process.env.API_CLOUDINARY_NAME}`,
     api_key: `${process.env.API_CLOUDINARY_API_KEY}`,
