31136e2765ccba4b54fb1ce754eb1d63ae879790
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-09-14 18:37:03 -0300

    feat: add create teacher use case

---
+++ src/application/use-cases/teacher/create-teacher-use-case.ts
@@ -0,0 +1,40 @@
+import { Teacher } from 'src/domain/entities/Teacher';
+import { TeacherRepository } from 'src/domain/repositories/TeacherRepository';
+
+interface CreateTeacherUseCaseRequest {
+  name: string;
+  email: string;
+  age: number;
+  training: string;
+  stacks: string[];
+  description: string;
+  photo: string;
+}
+
+interface CreateTeacherUseCaseResponse {
+  teacher: Teacher;
+}
+
+export class CreateTeacherUseCase {
+  constructor(private teacherRepository: TeacherRepository) {}
+
+  async execute(
+    request: CreateTeacherUseCaseRequest,
+  ): Promise<CreateTeacherUseCaseResponse> {
+    const { name, email, age, training, stacks, description, photo } = request;
+
+    const teacher = new Teacher({
+      name,
+      email,
+      age,
+      training,
+      stacks,
+      description,
+      photo,
+    });
+
+    return {
+      teacher,
+    };
+  }
+}
