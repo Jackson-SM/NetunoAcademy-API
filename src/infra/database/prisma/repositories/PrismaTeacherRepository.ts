8ccfd6c571a67047209ff72f36ab98aa07934fbe
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-09-16 22:27:38 -0300

    feat: add create teacher in prisma repo

--- src/infra/database/prisma/repositories/PrismaTeacherRepository.ts
+++ src/infra/database/prisma/repositories/PrismaTeacherRepository.ts
@@ -1,14 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Teacher } from 'src/domain/entities/Teacher';
 import { TeacherRepository } from 'src/domain/repositories/TeacherRepository';
+import { PrismaTeacherMapper } from '../mappers/PrismaTeacherMapper';
 import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class PrismaTeacherRepository implements TeacherRepository {
   constructor(private prisma: PrismaService) {}
 
-  create(teacher: Teacher): Promise<Teacher> {
-    throw new Error('Method not implemented.');
+  async create(teacher: Teacher): Promise<Teacher> {
+    const userExists = await this.prisma.teacher.count({
+      where: { email: teacher.email },
+    });
+
+    if (userExists > 0) {
+      throw new HttpException('User already exists', HttpStatus.CONFLICT);
+    }
+
+    const raw = await this.prisma.teacher.create({
+      data: PrismaTeacherMapper.toPrisma(teacher),
+    });
+
+    return PrismaTeacherMapper.toDomain(raw);
   }
 
   getById(id: string): Promise<Teacher> {
