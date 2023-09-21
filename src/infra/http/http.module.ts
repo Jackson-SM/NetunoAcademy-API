d93d49299b6d9a5ef39b854dd718246430b1ab86
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-09-20 23:45:28 -0300

    feat: add teacher module in http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './controllers/AuthController/auth.module';
-import { UserModule } from './controllers/UserController/user.module';
 import { CourseModule } from './controllers/CourseController/course.module';
+import { TeacherModule } from './controllers/TeacherController/teacher.module';
+import { UserModule } from './controllers/UserController/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule, CourseModule],
+  imports: [AuthModule, UserModule, CourseModule, TeacherModule],
   controllers: [],
   providers: [],
 })
